63f813a9a38c7680dbb4ef337856d2a9a20cf580
iGxnon <1545766400@qq.com>
2022-08-21 17:25:06 +0800

    init CI/CD

--- tools/routing_tool/tool.go
+++ tools/routing_tool/tool.go
@@ -6,10 +6,10 @@ import (
 	"strconv"
 )
 
-//MaintainRouting 维护协程直到回话结束，第一次则注册此协程
+// MaintainRouting 维护协程直到回话结束，第一次则注册此协程
 func MaintainRouting(senderId int64, id int64, msg string, repeated bool, flag string) error {
 	_senderIdStr := strconv.FormatInt(senderId, 10)
-	//信息包装
+	// 信息包装
 	var t = global.ChanMsg{
 		Id:       id,
 		Msg:      msg,
@@ -17,13 +17,13 @@ func MaintainRouting(senderId int64, id int64, msg string, repeated bool, flag s
 		Repeated: repeated,
 	}
 
-	//逻辑处理
+	// 逻辑处理
 	if l, ok := global.Routing[_senderIdStr]; ok {
 		l.Lock()
 		defer l.Unlock()
 		l.C <- &t
 	} else {
-		//最大连接数量把控
+		// 最大连接数量把控
 
 		if global.PoolNumber < global.MaxPoolNumber {
 			RegisterRouting(_senderIdStr, &t)
@@ -35,23 +35,23 @@ func MaintainRouting(senderId int64, id int64, msg string, repeated bool, flag s
 	return nil
 }
 
-//RegisterRouting 注册协程时不需要考虑是否为群聊
+// RegisterRouting 注册协程时不需要考虑是否为群聊
 func RegisterRouting(senderIdStr string, text *global.ChanMsg) {
 
 	var x global.Logic
 	global.Routing[senderIdStr] = &x
 
-	//守护进程
+	// 守护进程
 	global.Routing[senderIdStr].Lock()
 	defer global.Routing[senderIdStr].Unlock()
 
-	//执行默认函数--会阻塞一定的时间
+	// 执行默认函数--会阻塞一定的时间
 	err := global.Pool.Invoke(senderIdStr)
 	if err != nil {
 		return
 	}
 
-	//信息发送
+	// 信息发送
 	x.C = make(chan *global.ChanMsg)
 	x.C <- text
 }
